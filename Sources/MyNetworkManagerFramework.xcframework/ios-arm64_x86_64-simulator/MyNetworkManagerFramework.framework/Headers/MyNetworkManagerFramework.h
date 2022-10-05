e61b21a38500f7e8137481deadf6fd823418e957
Sarathkumar <27sarathkumar.s@gmail.com>
2022-10-05 15:49:29 +0530

    Framework added

---
+++ Sources/MyNetworkManagerFramework.xcframework/ios-arm64_x86_64-simulator/MyNetworkManagerFramework.framework/Headers/MyNetworkManagerFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyNetworkManagerFramework.h
+//  MyNetworkManagerFramework
+//
+//  Created by SarathKumar S on 05/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyNetworkManagerFramework.
+FOUNDATION_EXPORT double MyNetworkManagerFrameworkVersionNumber;
+
+//! Project version string for MyNetworkManagerFramework.
+FOUNDATION_EXPORT const unsigned char MyNetworkManagerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNetworkManagerFramework/PublicHeader.h>
+
+
